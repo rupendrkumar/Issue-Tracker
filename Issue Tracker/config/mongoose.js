4731bb740fd0493488ac774e255102f13cf13ea3
RUPENDRA KUMAR <140410383+rupendrkumar@users.noreply.github.com>
2024-01-13 04:10:32 +0530

    Update mongoose.js

--- Issue Tracker/config/mongoose.js
+++ Issue Tracker/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoURI = 'mongodb+srv://rupendrakumar223com:GRGszmTD7QMccTpp@cluster0.m6awew0.mongodb.net/';
+const mongoURI = 'mongodb+srv://rupendrakumar223com:GRGszmTD7QMccTpp@cluster0.m6awew0.mongodb.net/cluster0?retryWrites=true&w=majority';
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
